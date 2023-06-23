bb81839ade49b8fb295f986b652ab70496e7c64f
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-06-23 20:35:19 +0300

    Update hexlet.code.AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -9,7 +9,7 @@ public final class AppTest {
     private static final int EXIT_CODE_OK = 0;
     @Test
     public void appRightTest() {
-        int actual = new CommandLine(new App()).execute("file1.json", "file2.json");
-        assertThat(actual).isEqualTo(EXIT_CODE_OK);
+        int actual1 = new CommandLine(new App()).execute("file1.json", "file2.json");
+        assertThat(actual1).isEqualTo(EXIT_CODE_OK);
     }
 }
